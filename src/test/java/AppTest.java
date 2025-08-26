dae79ffc660278a593b863a8fd8ad878ea4efb0a
DanielMDexcom <daniel.moreno4@dexcom.com>
2025-08-26 10:56:22 -0700

    Initial setup

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AppTest {
+
+    @Test
+    void testAddition() {
+        assertEquals(5, 2 + 3, "2 + 3 debe ser 5");
+    }
+}
